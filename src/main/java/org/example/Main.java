b031cebb1f1e69ffb9d6378f50d92ee9e9659c4e
DrEvil <opc_senator4@mail.ru>
2022-10-22 23:46:51 +0300

    Первая версия программы

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+        long bonus = service.calculate(5_000, true);
+        System.out.println("получился бонус:" + bonus);
+    }
+}
